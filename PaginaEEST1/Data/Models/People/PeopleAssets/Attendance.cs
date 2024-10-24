ea5d32747e8e4d1238d57d3f5d3562bb0cba0a0a
Bernard2806 <107374580+Bernard2806@users.noreply.github.com>
2024-10-24 08:56:33 -0300

    Project structure removed to recreate without Entra ID and Entity Framework Core (MySQL)

--- PaginaEEST1/Data/Models/People/PeopleAssets/Attendance.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace PaginaEEST1.Data.Models.People.PeopleAssets
-{
-    public class Attendance
-    {
-        public int Id { get; set; }
-        public DateTime Date { get; set; }
-        public List<AttendanceRecord> Records { get; set; } = new List<AttendanceRecord>();
-    }
-}
--- PaginaEEST1/Data/Models/PhysicalObjects/PhysicalAssets/Request/InstallationRequest.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace PaginaEEST1.Data.Models.PhysicalObjects.PhysicalAssets.Request
-{
-    public class InstallationRequest : RequestComputer
-    {
-        // Detalles específicos de la instalación
-        public string NameProgram { get; set; } = null!;  // Nombre del programa a instalar
-        public string? VersionProgram { get; set; }  // Versión del programa a instalar (opcional)
-    }
-}
