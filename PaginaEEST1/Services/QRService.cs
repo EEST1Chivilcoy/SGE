74dbdc5726c215b30fd4b8690c904c32bacdb4ee
Bernard2806 <bgonzalezerramuspe@eest1chivilcoy.edu.ar>
2024-11-25 22:20:12 -0300

    Fix issue with QR not displaying in computer details component by integrating external QR API

--- PaginaEEST1/Services/QRService.cs
+++ PaginaEEST1/Services/QRService.cs
@@ -12,6 +12,7 @@ namespace PaginaEEST1.Services
     public interface IQRService
     {
         Task<string> LoadQR(string link);
+        string GetQRCodeUrl(string link);
     }
 
     public class QRService : IQRService
@@ -21,6 +22,7 @@ namespace PaginaEEST1.Services
         {
             _context = context;
         }
+
         public async Task<string> LoadQR(string link)
         {
             return await Task.Run(() =>
@@ -31,6 +33,17 @@ namespace PaginaEEST1.Services
                 byte[] qrCodeAsPngByteArr = qrCode.GetGraphic(20);
                 return $"data:image/png;base64,{Convert.ToBase64String(qrCodeAsPngByteArr)}";
             });
+        }
+
+        public string GetQRCodeUrl(string link)
+        {
+            // Reemplazar el espacio en blanco y otros caracteres especiales por su equivalente en URL
+            var encodedLink = Uri.EscapeDataString(link);
+
+            // Usamos la API de GoQR.me para obtener la URL del QR
+            var qrApiUrl = $"https://api.qrserver.com/v1/create-qr-code/?data={encodedLink}&size=200x200";
+
+            return qrApiUrl;
         }
     }
 }
